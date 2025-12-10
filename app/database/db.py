c6733dd0aaa635b07e5f0b3c270dc8f7f6e47ef1
Júlio <jc.5047792@gmail.com>
2025-12-09 20:58:29 -0400

    database module made

---
+++ app/database/db.py
@@ -0,0 +1,18 @@
+import mariadb
+from dotenv import load_dotenv
+import os
+
+ load_dotenv()
+
+ conn_params = {
+   "user": os.environ["DB_USER"],
+   "host": os.environ["DB_HOST"],
+   "database": os.environ["DB_NAME"],
+   "password": os.environ["DB_PASSWORD"]
+ }
+
+ pool = mariadb.ConnectionPool(
+   pool_name="main_pool",
+   pool_reset_connection=False,
+   **conn_params
+ )
